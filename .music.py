d21770e80131f55ddbd51e0301cd29af43ec327b
Vlad <vladstudio@rambler.ru>
2015-01-15 21:09:44 +0300

    init

---
+++ .music.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import subprocess, os, time
+
+out = subprocess.check_output(['ps', '-x'])
+out = str(out)[2:-3]
+
+if out.find('foobnix') != -1:
+	out = subprocess.check_output(['foobnix', '--state'])
+	out = str(out)[2:-3]
+
+	if out == 'STOP':
+		print('No music is playing')
+	if out == 'PLAY':
+		out = subprocess.check_output(['foobnix', '--now-playing'])
+		out = str(out)[2:-3]
+		print('►  ' + out)
+	if out == 'PAUSE':
+		out = subprocess.check_output(['foobnix', '--now-playing'])
+		out = str(out)[2:-3]
+		print('▍▍  ' + out)
+else:
+	print('No music player')
